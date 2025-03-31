fc6575b9753e6c0855b9effbc3f8659133656d48
Uday_Patel <148987243+Uday12704@users.noreply.github.com>
2025-03-31 20:46:33 +0530

    Update wsgi.py

--- movierecommender/wsgi.py
+++ movierecommender/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'movierecommender.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Movie-Recommender-System.settings')
 
 application = get_wsgi_application()
